b3ad27ffe2d4221f2a74e97d973cd253b2d465c2
Vinicius Andrade <viniciusy62@gmail.com>
2024-03-20 18:08:11 -0300

    updated inicial project and ThemeProvider added

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+export function Details() {
+  return (
+    <Container>
+      <h1>Details</h1>
+      <p>Esta é a página Details!</p>
+    </Container>
+  );
+}
---
+++ src/pages/Details/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  padding: 20px;
+  background: #222;
+  color: #fff;
+`;

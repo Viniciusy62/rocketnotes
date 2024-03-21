3ee179b697cf6a59aad1465b77b308e2585e69cc
Vinicius Andrade <viniciusy62@gmail.com>
2024-03-20 21:54:00 -0300

    correction on git merge

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-<<<<<<< HEAD
 
 import { ThemeProvider } from "styled-components";
 
 import GlobalStyles from "./styles/global";
-=======
-import { ThemeProvider } from "styled-components";
-
->>>>>>> refs/remotes/origin/main
 import theme from "./styles/theme";
 
 import { Details } from "./pages/Details";
@@ -16,10 +11,7 @@ import { Details } from "./pages/Details";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-<<<<<<< HEAD
       <GlobalStyles />
-=======
->>>>>>> refs/remotes/origin/main
       <Details />
     </ThemeProvider>
   </React.StrictMode>
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,11 +1,3 @@
 import styled from "styled-components";
 
-<<<<<<< HEAD
 export const Container = styled.div``;
-=======
-export const Container = styled.div`
-  padding: 20px;
-  background: #222;
-  color: #fff;
-`;
->>>>>>> refs/remotes/origin/main

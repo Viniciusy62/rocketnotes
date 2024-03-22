f8eaf74c6c29569fa5430cbccaa11aadec955cc8
Vinicius Andrade <viniciusy62@gmail.com>
2024-03-22 18:08:42 -0300

    create Button component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,13 @@
 import { Container } from "./styles";
+import { Button } from "../../components/Button";
+
 export function Details() {
   return (
     <Container>
       <h1>Details</h1>
       <p>Esta é a página Details!</p>
+      
+      <Button />
     </Container>
   );
 }
